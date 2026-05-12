36502c3473f4be04fb3a071adfacd5dd9ade1c1a
Manikrish52 <137646901+Manikrish52@users.noreply.github.com>
2026-05-12 12:23:59 +0530

    optimise the code.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Dashboard from './Compontents/Dashboard';
 import Router from './Compontents/Router';
 
 function App() {
--- my-app/src/Compontents/Dashboard.js
+++ my-app/src/Compontents/Dashboard.js
@@ -3,20 +3,22 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
 import {
-  Table, TableBody, TableCell, TableContainer,
-  TableHead, TableRow, Paper, Button,
-  TextField, Dialog, DialogTitle,
-  DialogContent, DialogActions,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  TextField,
   Grid,
-  colors,
   Typography,
   Switch,
   InputAdornment,
   IconButton,
-  Checkbox,
-  FormControlLabel,
   CircularProgress,
-  Card
+  Card,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import {
@@ -24,16 +26,15 @@ import {
   addEmployee,
   deleteEmployee,
   updateEmployee,
-  setStatus
+  setStatus,
 } from "../Redux/employeeSlice";
 import employees from "../Redux/employeeSlice";
 import EditPopup from "./EditPopup";
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 import ClearIcon from "@mui/icons-material/Clear";
 import Header from "./Header";
-import EmployeeProfile from "./EmployeeProfileList";
 import EmployeeCard from "./EmployeeCard";
 import BarChat from "./BarChat";
 const Dashboard = () => {
@@ -65,11 +66,9 @@ const Dashboard = () => {
     dispatch(fetchEmployees());
   }, [dispatch]);
 
-
-
   const totalEmployees = employees.length;
 
-  const activeCount = employees.filter(emp => emp.active).length;
+  const activeCount = employees.filter((emp) => emp.active).length;
   const inactiveCount = totalEmployees - activeCount;
 
   const departmentStats = employees.reduce((acc, emp) => {
@@ -78,7 +77,7 @@ const Dashboard = () => {
     return acc;
   }, {});
 
-  const recentActivities = employees.slice(0, 5).map(emp => ({
+  const recentActivities = employees.slice(0, 5).map((emp) => ({
     message: `${emp.name} loaded`,
   }));
   console.log("departmentStats:", departmentStats);
@@ -86,9 +85,8 @@ const Dashboard = () => {
   const filtered = employees.filter(
     (emp) =>
       emp.name.toLowerCase().includes(search.toLowerCase()) ||
-      emp.department.toLowerCase().includes(search.toLowerCase())
+      emp.department.toLowerCase().includes(search.toLowerCase()),
   );
-
 
   const handleSave = () => {
     if (editId) {
@@ -99,7 +97,7 @@ const Dashboard = () => {
           id: Date.now(),
           ...form,
           active: true,
-        })
+        }),
       );
     }
 
@@ -125,12 +123,30 @@ const Dashboard = () => {
     padding: "15px",
     borderRadius: "10px",
     boxShadow: "0 2px 10px rgba(0,0,0,0.1)",
-
   };
   const [deptFilter, setDeptFilter] = useState("");
-  const departments = [...new Set(employees.map(emp => emp.department))];
+  const departments = [...new Set(employees.map((emp) => emp.department))];
   const [selectedDepts, setSelectedDepts] = useState([]);
-  const cardHeaders = [{ id: "1", header: "Total Employees", color: "red", background: "linear-gradient(90deg, #ff416c, #ff4b2b)" }, { id: "2", header: "Active", color: "green", background: "linear-gradient(90deg, #00b09b, #96c93d)" }, { id: "3", header: "Inactive", color: "red", background: "linear-gradient(90deg, #ff416c, #ff4b2b)" }];
+  const cardHeaders = [
+    {
+      id: "1",
+      header: "Total Employees",
+      color: "red",
+      background: "linear-gradient(90deg, #ff416c, #ff4b2b)",
+    },
+    {
+      id: "2",
+      header: "Active",
+      color: "green",
+      background: "linear-gradient(90deg, #00b09b, #96c93d)",
+    },
+    {
+      id: "3",
+      header: "Inactive",
+      color: "red",
+      background: "linear-gradient(90deg, #ff416c, #ff4b2b)",
+    },
+  ];
 
   // loader
   const [visibleCount, setVisibleCount] = useState(10);
@@ -138,9 +154,7 @@ const Dashboard = () => {
   const loaderRef = useRef(null);
   const handleDeptChange = (dept) => {
     setSelectedDepts((prev) =>
-      prev.includes(dept)
-        ? prev.filter((d) => d !== dept)
-        : [...prev, dept]
+      prev.includes(dept) ? prev.filter((d) => d !== dept) : [...prev, dept],
     );
   };
 
@@ -150,9 +164,8 @@ const Dashboard = () => {
   const filteredEmployees = employees.filter(
     (emp) =>
       emp.name.toLowerCase().includes(search.toLowerCase()) ||
-      emp.department.toLowerCase().includes(search.toLowerCase())
+      emp.department.toLowerCase().includes(search.toLowerCase()),
   );
-
 
   const visibleEmployees = filteredEmployees.slice(0, visibleCount);
   const loadMore = () => {
@@ -173,7 +186,7 @@ const Dashboard = () => {
           loadMore();
         }
       },
-      { threshold: 1 }
+      { threshold: 1 },
     );
 
     if (loaderRef.current) {
@@ -183,24 +196,34 @@ const Dashboard = () => {
     return () => observer.disconnect();
   }, [filteredEmployees, visibleCount]);
 
-
   return (
-    <div >
-      <Header showPopup={showPopup} setShowPopup={setShowPopup} handleLogout={handleLogout} />
+    <div>
+      <Header
+        showPopup={showPopup}
+        setShowPopup={setShowPopup}
+        handleLogout={handleLogout}
+      />
       {/* employee card */}
-      <EmployeeCard totalEmployees={totalEmployees} activeCount={activeCount} inactiveCount={inactiveCount} cardHeaders={cardHeaders} />
+      <EmployeeCard
+        totalEmployees={totalEmployees}
+        activeCount={activeCount}
+        inactiveCount={inactiveCount}
+        cardHeaders={cardHeaders}
+      />
       {/* bar chart */}
-      <Grid container p={2} spacing={2} >
-        <Grid item xs={12} md={6}  >
+      <Grid container p={2} spacing={2}>
+        <Grid item xs={12} md={6}>
           <Card
             sx={{
               border: "1px solid #e0e0e0",
               boxShadow: "0 4px 12px rgba(0,0,0,0.08)",
               borderRadius: "12px",
-              padding: "10px"
+              padding: "10px",
             }}
           >
-            <Typography sx={{ fontWeight: 600, fontSize: "16px" }}>Departments</Typography>
+            <Typography sx={{ fontWeight: 600, fontSize: "16px" }}>
+              Departments
+            </Typography>
             <BarChat dataSource={departmentStats} type="department" />
           </Card>
         </Grid>
@@ -211,23 +234,27 @@ const Dashboard = () => {
               border: "1px solid #e0e0e0",
               boxShadow: "0 4px 12px rgba(0,0,0,0.08)",
               borderRadius: "12px",
-              padding: "10px"
-            }}  >
-
-            <Typography sx={{ fontWeight: 600, fontSize: "16px" }}>Recent Activities</Typography>
+              padding: "10px",
+            }}
+          >
+            <Typography sx={{ fontWeight: 600, fontSize: "16px" }}>
+              Recent Activities
+            </Typography>
             <BarChat dataSource={recentActivities} type="activity" />
           </Card>
         </Grid>
       </Grid>
       {/* search and add employee controls */}
-      <Grid sx={{
-        marginTop: "20px",
-        marginLeft: "20px",
-        display: "flex",
-        gap: "10px",
-        marginBottom: "20px",
-        height: "30px"
-      }}>
+      <Grid
+        sx={{
+          marginTop: "20px",
+          marginLeft: "20px",
+          display: "flex",
+          gap: "10px",
+          marginBottom: "20px",
+          height: "30px",
+        }}
+      >
         <TextField
           className="Textfield_content"
           label="Search Employee"
@@ -244,7 +271,15 @@ const Dashboard = () => {
           }}
         />
 
-        <Button variant="contained" onClick={handleAdd} sx={{ textTransform: "none", height: "37px", background: "linear-gradient(135deg, #0f2027, #203a43, #2c5364)" }} >
+        <Button
+          variant="contained"
+          onClick={handleAdd}
+          sx={{
+            textTransform: "none",
+            height: "37px",
+            background: "linear-gradient(135deg, #0f2027, #203a43, #2c5364)",
+          }}
+        >
           Add Employee
         </Button>
       </Grid>
@@ -264,78 +299,84 @@ const Dashboard = () => {
           </TableHead>
 
           {!loading ? (
-            <TableBody >
+            <TableBody>
               {visibleEmployees.length === 0 ? (
                 <TableRow>
                   <TableCell colSpan={5}>No data found</TableCell>
                 </TableRow>
-              ) : visibleEmployees.map((emp) => (
-                <TableRow key={emp.id}>
-                  <TableCell>{emp.name}</TableCell>
-                  <TableCell>{emp.email}</TableCell>
-                  <TableCell>{emp.department}</TableCell>
+              ) : (
+                visibleEmployees.map((emp) => (
+                  <TableRow key={emp.id}>
+                    <TableCell>{emp.name}</TableCell>
+                    <TableCell>{emp.email}</TableCell>
+                    <TableCell>{emp.department}</TableCell>
 
-                  <TableCell>
-                    <span style={{ marginRight: "8px" }}>
-                      {emp.active ? "Active" : "Inactive"}
-                    </span>
+                    <TableCell>
+                      <span style={{ marginRight: "8px" }}>
+                        {emp.active ? "Active" : "Inactive"}
+                      </span>
 
-                    <Switch
-                      checked={emp.active}
-                      onChange={(e) =>
-                        dispatch(setStatus({ id: emp.id, status: e.target.checked }))
-                      }
-                      color="success"
-                    />
-                  </TableCell>
-
-                  <TableCell>
-                    <Button
-                      className="btn_content"
-                      onClick={() => handleView(emp)}
-                      title="View"
-                    >
-                      <VisibilityIcon />
-                    </Button>
-                    <Button
-                      className="btn_content"
-                      title="edit"
-                      onClick={() => {
-                        setForm(emp);
-                        setEditId(emp.id);
-                        setOpen(true);
-                        setIsViewMode(false);
-                      }}
-                    >
-                      <EditIcon />
-                    </Button>
-
-                    <Button
-                      title="Delete"
-                      className="btn_content"
-                      color="error"
-                      onClick={() => {
-                        const confirmDelete = window.confirm("Are you sure you want to delete this user?");
-                        if (confirmDelete) {
-                          dispatch(deleteEmployee(emp.id));
+                      <Switch
+                        checked={emp.active}
+                        onChange={(e) =>
+                          dispatch(
+                            setStatus({ id: emp.id, status: e.target.checked }),
+                          )
                         }
-                      }}
-                    >
-                      <DeleteIcon />
-                    </Button>
-                  </TableCell>
-                </TableRow>
+                        color="success"
+                      />
+                    </TableCell>
 
-              ))}
+                    <TableCell>
+                      <Button
+                        className="btn_content"
+                        onClick={() => handleView(emp)}
+                        title="View"
+                      >
+                        <VisibilityIcon />
+                      </Button>
+                      <Button
+                        className="btn_content"
+                        title="edit"
+                        onClick={() => {
+                          setForm(emp);
+                          setEditId(emp.id);
+                          setOpen(true);
+                          setIsViewMode(false);
+                        }}
+                      >
+                        <EditIcon />
+                      </Button>
+
+                      <Button
+                        title="Delete"
+                        className="btn_content"
+                        color="error"
+                        onClick={() => {
+                          const confirmDelete = window.confirm(
+                            "Are you sure you want to delete this user?",
+                          );
+                          if (confirmDelete) {
+                            dispatch(deleteEmployee(emp.id));
+                          }
+                        }}
+                      >
+                        <DeleteIcon />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))
+              )}
             </TableBody>
           ) : (
             <TableBody>
               <TableRow>
-                <TableCell colSpan={5}><CircularProgress /></TableCell>
+                <TableCell colSpan={5}>
+                  <CircularProgress />
+                </TableCell>
               </TableRow>
             </TableBody>
           )}
-
         </Table>
       </TableContainer>
       <div
@@ -343,13 +384,21 @@ const Dashboard = () => {
         style={{
           height: "60px",
           textAlign: "center",
-          padding: "10px"
+          padding: "10px",
         }}
       >
         {loadingMore && <p>Loading more employees...</p>}
       </div>
 
-      <EditPopup open={open} onClose={() => setOpen(false)} editId={editId} form={form} setForm={setForm} handleSave={handleSave} isViewMode={isViewMode} />
+      <EditPopup
+        open={open}
+        onClose={() => setOpen(false)}
+        editId={editId}
+        form={form}
+        setForm={setForm}
+        handleSave={handleSave}
+        isViewMode={isViewMode}
+      />
     </div>
   );
 };
